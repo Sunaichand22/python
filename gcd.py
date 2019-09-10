117cc746960db5bdd365d1a73a46480bbad689d0
Sunaichand22 <49715853+Sunaichand22@users.noreply.github.com>
2019-09-10 08:52:53 +0530

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,10 @@
+num1,num2=map(int,input().split())
+if(num1<num2):
+   small=num1
+else:
+   small=num2
+gcd=1
+for i in range(1,small+1):
+   if(num1%i==0 and num2%i==0):
+      gcd=gcd*i
+print(gcd)
